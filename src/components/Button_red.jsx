292ad1be744430947ff4fefa25f44085fa6f05af
zoiska <zoisk64@gmail.com>
2026-04-24 01:10:20 +0200

    change gray button name for component usecase clarity, add type prop to red button

--- src/components/Button_red.jsx
+++ src/components/Button_red.jsx
@@ -1,7 +1,7 @@
-const Button_red = ({ text }) => {
+const Button_red = ({ text, type }) => {
   return (
     <button
-      type="submit"
+      type={type}
       className="border-1 border-red-600 bg-red-600 text-white font-bold rounded-sm p-2 hover:bg-zinc-900 active:bg-red-00"
     >
       {text}
