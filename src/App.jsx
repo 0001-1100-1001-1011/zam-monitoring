3fb05fc5b055dc9d1b301ace97e5ce2bc55e05c6
MarcoEbel <marco-ebel-91@web.de>
2026-04-24 14:22:03 +0200

    userpage and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { Navigate, Route, Routes } from "react-router";
 import Login from "./pages/Login.jsx";
+import Users from "./pages/Users.jsx";
+
 
 function App() {
   return (
@@ -7,9 +9,11 @@ function App() {
       <Routes>
         <Route path="*" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/user" element={<Users />} />
       </Routes>
     </>
   );
 }
+
 
 export default App;
---
+++ src/pages/Users.jsx
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react";
+import UserTable from "../Components/UserTable.jsx";
+
+
+export default function Users() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    // Dummy-Daten
+    const dummy = [
+      {
+        id: 1,
+        name: "Marco",
+        role: "Planlos",
+        status: "online",
+        lastLogin: "2026-04-23",
+        time:  "18:22",
+      },
+      {
+        id: 2,
+        name: "Alik",
+        role: "Macher",
+        status: "offline",
+        lastLogin: "2026-04-22",
+        time:  "09:14",
+      },
+      {
+        id: 3,
+        name: "Zoisk",
+        role: "Gott",
+        status: "Abwesend",
+        lastLogin: "2026-04-23",
+        time:  "20:01",
+      },
+    ];
+    setUsers(dummy);
+  }, []);
+
+  return (
+    <div className="min-h-screen bg-[var(--bg)] text-white flex flex-col items-center py-16">
+      <div className="border-4 border-red-600 bg-zinc-800 rounded-3xl p-10 w-[600px] space-y-8">
+        <h1 className="text-3xl font-bold text-center text-red-500">
+          Benutzer
+        </h1>
+        <p className="text-center text-gray-300">
+          Aktive Benutzer, Rollen und letzte Aktivitäten.
+        </p>
+
+        <UserTable users={users} />
+      </div>
+    </div>
+  );
+}
