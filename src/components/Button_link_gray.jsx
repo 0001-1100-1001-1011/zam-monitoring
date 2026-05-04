4edfbf38a48c5783437a96d5bd3fd953fdc1cdeb
Zois Karagiannis <zoisk64@gmail.com>
2026-05-04 13:13:37 -0400

    re-fix components folder, fix button link prop

--- src/components/Button_link_gray.jsx
+++ src/components/Button_link_gray.jsx
@@ -1,7 +1,7 @@
 const Button_gray = ({ text, link }) => {
   return (
     <button
-      onClick={() => window.open({ link }, "_blank")}
+      onClick={() => window.open(link, "_blank")}
       className="border border-red-600 bg-zinc-800 text-white font-bold rounded-sm p-2 hover:bg-red-600 active:bg-red-500"
     >
       {text}
