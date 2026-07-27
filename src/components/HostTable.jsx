8360d4952a9f99218970afc4e6388e62954d0a12
MarcoEbel <marco-ebel-91@web.de>
2026-07-27 22:13:56 +0200

    Changed Hoteltable to be in Line with Database

--- src/components/HostTable.jsx
+++ src/components/HostTable.jsx
@@ -2,11 +2,15 @@ import AllTables from "./AllTables.jsx";
 
 export default function HostTable({ hosts }) {
   const columns = [
-    { key: "Hostname", label: "Hostname" },
-    { key: "role", label: "Role" },
-    { key: "status", label: "Status" },
-    { key: "lastOnline", label: "Last Online" },
-    { key: "cpu", label: "CPU" },
+    { key: "hostname", label: "Hostname" },
+    { key: "ip_address", label: "IP" },
+    { key: "cpu_model", label: "CPU" },
+    { key: "ram_size", label: "Arbeitspeicher" },
+    { key: "gpu_model", label: "Grafikkarte" },
+    { key: "storage_size", label: "Speichergröße" },
+    { key: "operating_system", label: "OS" },
+    { key: "last_seen", label: "Letzter Log" },
+    //{ key: "status", label: "Status" }, nice to have
   ];
 
   return <AllTables columns={columns} data={hosts} />;
