5c53bb74f746e1815989338be28e1bdbb58ec1ed
MarcoEbel <marco-ebel-91@web.de>
2026-08-29 13:53:17 +0200

    removed Sidebar

--- src/pages/Hosts.jsx
+++ src/pages/Hosts.jsx
@@ -1,6 +1,8 @@
 import HostTable from "../components/HostTable.jsx";
 import HeaderNavigation from "../components/HeaderNavigation.jsx";
-import Sidebar from "../components/Sidebar.jsx";
+{
+  /*import Sidebar from "../components/Sidebar.jsx";*/
+}
 import { useHosts } from "../hooks/useHosts.jsx";
 
 export default function Hosts() {
@@ -9,7 +11,7 @@ export default function Hosts() {
   return (
     <>
       <HeaderNavigation />
-      <Sidebar />
+      {/*<Sidebar />*/}
 
       <div className="min-h-screen bg-(--bg) text-white flex flex-col items-center py-16">
         {error && (
@@ -22,7 +24,9 @@ export default function Hosts() {
 
         <div className="border-4 border-red-600 bg-zinc-800 rounded-3xl p-10 space-y-8">
           <h1 className="text-3xl font-bold text-center text-red-500">Hosts</h1>
-          <p className="text-center text-gray-300">Aktive Hosts, Rollen und letzte Aktivitäten.</p>
+          <p className="text-center text-gray-300">
+            Aktive Hosts, Rollen und letzte Aktivitäten.
+          </p>
 
           <HostTable hosts={hosts} />
         </div>
