b7426a69de4fbc792e6001a1933631df0d3adb0f
MarcoEbel <marco-ebel-91@web.de>
2026-08-29 13:55:44 +0200

    no really,  removed Sidebar

--- src/pages/LogsApplication.jsx
+++ src/pages/LogsApplication.jsx
@@ -1,6 +1,8 @@
 import { useNavigate } from "react-router";
 import LogsTable from "../components/LogsTable.jsx";
-import Sidebar from "../components/Sidebar.jsx";
+{
+  /*import Sidebar from "../components/Sidebar.jsx";*/
+}
 import HeaderNavigation from "../components/HeaderNavigation.jsx";
 import { useLogs } from "../hooks/useLogs.jsx";
 
@@ -19,7 +21,7 @@ export default function LogsApplication() {
 
   return (
     <>
-      <Sidebar />
+      {/*<Sidebar />*/}
       <HeaderNavigation />
       <div className="min-h-screen bg-(--bg) text-white flex flex-col items-center py-16 px-4">
         <div className="w-[900px] space-y-6">
